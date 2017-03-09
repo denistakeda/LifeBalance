38d067b837a0081db378039afb267034ffc642ab
Denis Krivosheev <denis.takeda@gmail.com>
2017-03-09 17:41:36 +0500

    feat(user): put current user to the context
    Closes #4

--- server/db/mongo/controllers/users.js
+++ server/db/mongo/controllers/users.js
@@ -1,13 +1,22 @@
 import R from 'ramda';
 import User from '../models/user';
-import {then} from '../../../utils/invokers';
+import {then, catchErr} from '../../../utils/invokers';
 import jwt from 'jsonwebtoken';
 import {SECRET_KEY} from '../../../../config/env';
 
-const generateToken = user => ({
-  user,
-  token: jwt.sign(user._id, SECRET_KEY)
-});
+export const extractUserMiddleware = (req, res, next) => R.pipe(
+  getToken,
+  getUserIdByToken,
+  userId => User.findById(userId),
+  then(
+    user => {
+      if (!user) next("Wrong token");
+      req.user = user;
+      next();
+    }
+  ),
+  catchErr(err => next(err))
+)(req);
 
 export const signUp = (_, args) => R.pipe(
   // TODO: verify the args
@@ -20,3 +29,16 @@ export const signIn = (_, args) => R.pipe(
   then(User.comparePassword(args.password)),
   then(generateToken)
 )(args);
+
+export const me = (_, __, context) => context.user;
+
+// Private part
+
+const generateToken = user => ({
+  user,
+  token: jwt.sign(user._id.toString(), SECRET_KEY)
+});
+
+const getUserIdByToken = token => jwt.verify(token, SECRET_KEY);
+
+const getToken = req => req.get('token');

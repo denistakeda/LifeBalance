901fd2c52cbd475c50436230cbbcc34a08c0bfed
Anton Gorbunov <anton.gorbunov.ru@gmail.com>
2017-03-17 19:33:09 +0500

    chore(tests): remove karma

--- config/tests/tests.webpack.js
+++
@@ -1,4 +0,0 @@
-const importAll = r => r.keys().forEach(r);
-
-// require.context(directory, useSubdirectories = false, regExp = /^\.\//)
-importAll(require.context('../../server', true, /\.spec\.js$/));

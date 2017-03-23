2a5ac69529360b66675f03353d46e6d0bfb35e8a
Denis Krivosheev <denis.takeda@gmail.com>
2017-03-23 16:21:37 +0500

    fix(env): use host and port instead of hardcoded string

--- config/env.js
+++ config/env.js
@@ -2,9 +2,9 @@ export const HOST = process.env.HOSTNAME || 'localhost';
 export const PORT = process.env.PORT || '3000';
 export const ENV = process.env.NODE_ENV || 'development';
 export const BACKEND_URL = process.env.BACKEND_URL ||
-  (ENV === 'development' ? 'http://localhost:3000/graphql' : 'https://life-balance.herokuapp.com/graphql');
+  (ENV === 'development' ? `http://${HOST}:${PORT}/graphql` : 'https://life-balance.herokuapp.com/graphql');
 
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || null;
 
-export const SECRET_KEY = process.env.SECRET_KEY || "Super Secret";
+export const SECRET_KEY = process.env.SECRET_KEY || 'Super Secret';
 
